7e196e98199798049946232530c7ad4de9f99965
DHURAIRAJ83 <dhurairaj83@gmail.com>
2025-07-04 11:09:02 +0530

    Update amazon.py

--- scraper/amazon.py
+++ scraper/amazon.py
@@ -1,3 +1,15 @@
-
-def scrape_amazon(product):
-    return [{"title": f"{product} - Amazon", "price": "₹250", "link": "https://amazon.in"}]
+def scrape_amazon(product: str):
+    return [
+        {
+            "product": f"{product} - Soft Cotton Amazon",
+            "price": "₹250",
+            "link": "https://www.amazon.in/dp/example",
+            "website": "Amazon"
+        },
+        {
+            "product": f"{product} - Premium Towel Amazon",
+            "price": "₹240",
+            "link": "https://www.amazon.in/dp/example2",
+            "website": "Amazon"
+        }
+    ]
