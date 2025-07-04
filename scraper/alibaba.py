be59e88e18a2c5191f4093196bc6b07e19eb7b3b
DHURAIRAJ83 <dhurairaj83@gmail.com>
2025-07-04 11:08:25 +0530

    Update alibaba.py

--- scraper/alibaba.py
+++ scraper/alibaba.py
@@ -1,3 +1,9 @@
-
-def scrape_alibaba(product):
-    return [{"title": f"{product} - Alibaba", "price": "$3", "link": "https://alibaba.com"}]
+def scrape_alibaba(product: str):
+    return [
+        {
+            "product": f"{product} - Alibaba Bulk Deal",
+            "price": "$3",
+            "link": "https://www.alibaba.com/product-detail/example",
+            "website": "Alibaba"
+        }
+    ]
